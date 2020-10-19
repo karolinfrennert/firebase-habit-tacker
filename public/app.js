018ecddead8c4da0336a4bd8f7949990cd37ad18
Karolin Frennert <karolinfrennert@gmail.com>
2020-10-19 14:42:05 +0200

    App

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
-console.log(firebase)
+const auth = firebase.auth();
+console.log(auth);
+//Selectors 
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutbtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutbtn.onclick = () => auth.signOut();
