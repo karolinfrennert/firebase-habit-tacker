0cd8b4adbec4a1953996aaaf6374d45a292c04fc
Hamed Khomjani <64583620+hamedkhomjani@users.noreply.github.com>
2020-10-22 19:37:02 +0200

    Wrote codes to make days appear when habit entered

--- public/app.js
+++ public/app.js
@@ -19,17 +19,17 @@ signInBtn.onclick = () => auth.signInWithPopup(provider);
 signOutbtn.onclick = () => auth.signOut();
 
 auth.onAuthStateChanged(user => {
-  if (user) {
-    //signed in
-    whenSignedIn.hidden = false;
-    whenSignedOut.hidden = true;
-    userDetails.innerHTML = `<h3> Hello ${user.displayName}!</h3> `;
-  } else {
-    //not signed in
-    whenSignedIn.hidden = true;
-    whenSignedOut.hidden = false;
-    userDetails.innerHTML = "";
-  }
+    if (user) {
+        //signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3> Hello ${user.displayName}!</h3> `;
+    } else {
+        //not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = "";
+    }
 });
 
 const db = firebase.firestore();
@@ -41,27 +41,44 @@ let habitsRef;
 let unsubsribe;
 
 auth.onAuthStateChanged(user => {
-  if (user) {
-    habitsRef = db.collection("habits");
+    if (user) {
+        habitsRef = db.collection("habits");
 
-    addHabit.onclick = (e) => {
-      e.preventDefault()
-      const { serverTimestamp } = firebase.firestore.FieldValue;
+        addHabit.onclick = (e) => {
+            e.preventDefault()
+            const { serverTimestamp } = firebase.firestore.FieldValue;
 
-      habitsRef.add({
-        uid: user.uid,
-        name: faker.commerce.productName(),
-        createdAt: serverTimestamp()
-      });
-    };
+            habitsRef.add({
+                uid: user.uid,
+                name: faker.commerce.productName(),
+                createdAt: serverTimestamp()
+            });
+        };
 
-    unsubscribe = habitsRef
-      .where("uid", "==", user.uid)
-      .onSnapshot(querySnapshot => {
-        const items = querySnapshot.docs.map(doc => {
-          return `<li>${doc.data().name}</li>`;
+        unsubscribe = habitsRef
+            .where("uid", "==", user.uid)
+
+        /*add function below*/
+        .onSnapshot(querySnapshot => {
+
+            /*past the days' name here*/
+
+            if (querySnapshot.docs.length) {
+                document.querySelector('#habitsTableHead').innerHTML = `<div class="habit-holder habit-holder-habit">Habit</div>
+                    <div class="habit-holder">Mon</div>
+                    <div class="habit-holder">Tue</div>
+                    <div class="habit-holder">Wed</div>
+                    <div class="habit-holder">Thu</div>
+                    <div class="habit-holder">Fri</div>
+                    <div class="habit-holder">Sat</div>
+                    <div class="habit-holder">Sun</div>`
+            }
+            /*  ^^^^^^  */
+
+            const items = querySnapshot.docs.map(doc => {
+                return `<li>${doc.data().name}</li>`;
+            });
+            habitsList.innerHTML = items.join("");
         });
-        habitsList.innerHTML = items.join("");
-      });
-  }
+    }
 });
---
+++ public/dist/app.dev.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var auth = firebase.auth(); //Selectors
+
+var whenSignedIn = document.getElementById("whenSignedIn");
+var whenSignedOut = document.getElementById("whenSignedOut");
+var signInBtn = document.getElementById("signInBtn");
+var signOutbtn = document.getElementById("signOutBtn");
+var userDetails = document.getElementById("userDetails");
+var provider = new firebase.auth.GoogleAuthProvider(); //input field
+//const inputHabit = document.getElementById('input-habit');
+
+signInBtn.onclick = function () {
+  return auth.signInWithPopup(provider);
+};
+
+signOutbtn.onclick = function () {
+  return auth.signOut();
+};
+
+auth.onAuthStateChanged(function (user) {
+  if (user) {
+    //signed in
+    whenSignedIn.hidden = false;
+    whenSignedOut.hidden = true;
+    userDetails.innerHTML = "<h3> Hello ".concat(user.displayName, "!</h3> ");
+  } else {
+    //not signed in
+    whenSignedIn.hidden = true;
+    whenSignedOut.hidden = false;
+    userDetails.innerHTML = "";
+  }
+});
+var db = firebase.firestore();
+var addHabit = document.getElementById("addHabit");
+var habitsList = document.getElementById("habitsList");
+var habitsRef;
+var unsubsribe;
+auth.onAuthStateChanged(function (user) {
+  if (user) {
+    habitsRef = db.collection("habits");
+
+    addHabit.onclick = function (e) {
+      e.preventDefault();
+      var serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
+      habitsRef.add({
+        uid: user.uid,
+        name: faker.commerce.productName(),
+        createdAt: serverTimestamp()
+      });
+    };
+
+    unsubscribe = habitsRef.where("uid", "==", user.uid)
+    /*add function below*/
+    .onSnapshot(function (querySnapshot) {
+      /*past the days' name here*/
+      if (querySnapshot.docs.length) {
+        document.querySelector('#habitsTableHead').innerHTML = "<div class=\"habit-holder habit-holder-habit\">Habit</div>\n                    <div class=\"habit-holder\">Mon</div>\n                    <div class=\"habit-holder\">Tue</div>\n                    <div class=\"habit-holder\">Wed</div>\n                    <div class=\"habit-holder\">Thu</div>\n                    <div class=\"habit-holder\">Fri</div>\n                    <div class=\"habit-holder\">Sat</div>\n                    <div class=\"habit-holder\">Sun</div>";
+      }
+      /*  ^^^^^^  */
+
+
+      var items = querySnapshot.docs.map(function (doc) {
+        return "<li>".concat(doc.data().name, "</li>");
+      });
+      habitsList.innerHTML = items.join("");
+    });
+  }
+});
